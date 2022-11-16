d65ddf5c85d900d5f11d84649d206edd51f83a76
Jay <jaykapadia389@gmail.com>
2022-11-16 17:18:36 +0530

    16/11/22

---
+++ 2D_array_io.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int m,n;
+cin >> n >>m ;
+int arr[m][n];
+
+for(int i=0;i<m;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        cin>> arr[i][j];
+    }       
+}
+cout << "the matrix is:\n" ;
+for(int i=0;i<m;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        cout << arr[i][j] <<" ";
+
+    }
+    cout << "\n";
+}
+}
